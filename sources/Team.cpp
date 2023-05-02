d51eeb43f8489e8f0a346191616dbf37fc1e03fd
IbrahemHurani <86603326+IbrahemHurani@users.noreply.github.com>
2023-05-02 12:27:50 +0300

    Add files via upload

---
+++ sources/Team.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include "Team.hpp"
+
+using namespace std;
+namespace ariel
+{
+    Team::Team(Character &lead) : leader(lead)
+    {
+    }
+    void Team::add(Character &new_warrior)
+    {
+    }
+    void Team::attack(Team &enemy)
+    {
+    }
+    int Team::stillAlive()
+    {
+        return 0;
+    }
+    void Team::print()
+    {
+    }
+
+};
